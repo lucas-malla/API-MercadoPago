c993fe7d6ea118e9c53672c36be290260b473e9e
lucas-malla <lucasarien.9210@gmail.com>
2022-07-18 12:27:53 -0300

    integrating-mercadoPago

--- controllers/checkout.controller.js
+++ controllers/checkout.controller.js
@@ -1,10 +1,14 @@
-//const mp = require('mp');
+const mp = require('../modules/mercadoPago');
 module.exports = {
     // Step 8
     process: async (req,res) => {
         try {
-            return res.send('Enviamos los datos a Mercado Pago')
+            let items = req.session.cart.map(item => Object({...item,currency_id:'ARS', unit_price:item.price}))
+            let link = await mp(items,12,0)
+            // link = link.body.init_point
+            return res.json(link.body)
         } catch (error) {
+            console.log('Mensaje');
             return res.send(error)
         }
     },
